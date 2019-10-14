93fe2f730849564aea0f55f2f4ee07f721ef5d4a
Frode Nordahl <frode.nordahl@canonical.com>
2019-10-14 15:01:29 +0200

    Add add_br and add_port helpers
    Move `run` helper from class up to the module level helper `_run`
    
    Change-Id: Ib338722f9a688b70ad06f4c445520133904d6ffc

--- src/lib/charm/ovsdb.py
+++ src/lib/charm/ovsdb.py
@@ -2,6 +2,56 @@ import json
 import subprocess
 
 
+def _run(*args):
+    """Run a process, check result, capture decoded output from STDERR/STDOUT.
+
+    :param args: Command and arguments to run
+    :type args: Union
+    :returns: Information about the completed process
+    :rtype: subprocess.CompletedProcess
+    :raises subprocess.CalledProcessError
+    """
+    return subprocess.run(
+        args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, check=True,
+        universal_newlines=True)
+
+
+def add_br(bridge, external_id=None):
+    """Add bridge and optionally attach a external_id to bridge.
+
+    :param bridge: Name of bridge to create
+    :type bridge: str
+    :param external_id: Key-value pair
+    :type external_id: Option[None,Union[str,str]]
+    :raises: subprocess.CalledProcessError
+    """
+    cmd = ['ovs-vsctl', 'add-br', bridge]
+    if external_id:
+        cmd.extend(('--', 'br-set-external-id'))
+        cmd.extend(external_id)
+    _run(*cmd)
+
+
+def add_port(bridge, port, external_id=None):
+    """Add port to bridge and optionally attach a external_id to port.
+
+    :param bridge: Name of bridge to attach port to
+    :type bridge: str
+    :param port: Name of port as represented in netdev
+    :type port: str
+    :param external_id: Key-value pair
+    :type external_id: Option[None,Union[str,str]]
+    :raises: subprocess.CalledProcessError
+    """
+    _run('ovs-vsctl', 'add-port', bridge, port)
+    if external_id:
+        ports = SimpleOVSDB('ovs-vsctl', 'port')
+        for port in ports.find('name={}'.format(port)):
+            ports.set(port['_uuid'],
+                      'external_ids:{}'.format(external_id[0]),
+                      external_id[1])
+
+
 class SimpleOVSDB(object):
     """Simple interface to OVSDB through the use of command line tools.
 
@@ -35,17 +85,11 @@ class SimpleOVSDB(object):
         self.tool = tool
         self.tbl = table
 
-    def run(self, *args):
-        cp = subprocess.run(
-            args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, check=True,
-            universal_newlines=True)
-        return cp
-
     def _find_tbl(self, condition=None):
         cmd = [self.tool, '-f', 'json', 'find', self.tbl]
         if condition:
             cmd.append(condition)
-        cp = self.run(*cmd)
+        cp = _run(*cmd)
         data = json.loads(cp.stdout)
         for row in data['data']:
             values = []
@@ -60,13 +104,13 @@ class SimpleOVSDB(object):
         return self._find_tbl()
 
     def clear(self, rec, col):
-        self.run(self.tool, 'clear', self.tbl, rec, col)
+        _run(self.tool, 'clear', self.tbl, rec, col)
 
     def find(self, condition):
         return self._find_tbl(condition=condition)
 
     def remove(self, rec, col, value):
-        self.run(self.tool, 'remove', self.tbl, rec, col, value)
+        _run(self.tool, 'remove', self.tbl, rec, col, value)
 
     def set(self, rec, col, value):
-        self.run(self.tool, 'set', self.tbl, rec, '{}={}'.format(col, value))
+        _run(self.tool, 'set', self.tbl, rec, '{}={}'.format(col, value))
