24f65d1d246ad621f52409970f754f0b366d8fdb
Frode Nordahl <frode.nordahl@canonical.com>
2020-03-19 08:09:49 +0100

    Use upstream subordinate release selection code
    The charm provided interim solution gives inconsistent results
    and is currently prohibiting deployment on Train.
    
    Also activate default release selector code in the concrete charm
    class module to make sure it is ready as on entry.
    
    Depends-On: Ieae81619e06dc287c2c42ec944425be4de13d0d6
    Change-Id: I51f1a81953b9dae85845fe390c72886f65117999

--- src/lib/charm/openstack/ovn_chassis.py
+++ src/lib/charm/openstack/ovn_chassis.py
@@ -11,66 +11,32 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import inspect
 
-import charmhelpers.fetch as ch_fetch
+import charms_openstack.charm as charm
 
 import charms.ovn_charm
 
 
-class OVNSubordinateChassisCharm(object):
-    """Mix-in for subordinate-specific handling."""
-
-    @staticmethod
-    def get_os_codename_package(package, codenames, fatal=True):
-        """Override to conditionally decide based on packages in apt cache.
-
-        Please inspect parent method for documentation of parameters.
-
-        The parent method also requires a package to already be installed,
-        and on initial charm deploy the fall back is to retrieve information
-        from a source configuration option.
-
-        Since we are a subordinate charm, we do not have a source configuration
-        option, thus we want to make the decision based on what is currently
-        available in the system APT cache alone. Our principle charm should
-        already have configured the system with the desired UCA pocket enabled.
-        """
-        # When the method is called outside of the context of the select
-        # release handler we do want it to assert presence of an installed
-        # package, as it is then used to detect available upgrades.
-        if (not inspect.currentframe().f_back.f_code.co_name == ''
-                'default_select_release'):
-            return super().get_os_codename_package(
-                package, codenames, fatal=fatal)
-
-        cache = ch_fetch.apt_cache()
-        try:
-            pkg = cache[package]
-        except KeyError:
-            if not fatal:
-                return
-            raise ValueError(
-                'Could not determine version of package with no installation '
-                'candidate: {}'.format(package))
-        ver = ch_fetch.apt_pkg.upstream_version(pkg.version)
-        major_ver = ver.split('.')[0]
-        if (package in codenames and
-                major_ver in codenames[package]):
-            return codenames[package][major_ver]
+charm.use_defaults('charm.default-select-release')
 
 
-class TrainOVNChassisCharm(OVNSubordinateChassisCharm,
-                           charms.ovn_charm.BaseTrainOVNChassisCharm):
+class TrainOVNChassisCharm(charms.ovn_charm.BaseTrainOVNChassisCharm):
     # OpenvSwitch and OVN is distributed as part of the Ubuntu Cloud Archive
     # Pockets get their name from OpenStack releases
     release = 'train'
     name = 'ovn-chassis'
 
+    # Setting an empty source_config_key activates special handling of release
+    # selection suitable for subordinate charms
+    source_config_key = ''
 
-class UssuriOVNChassisCharm(OVNSubordinateChassisCharm,
-                            charms.ovn_charm.BaseUssuriOVNChassisCharm):
+
+class UssuriOVNChassisCharm(charms.ovn_charm.BaseUssuriOVNChassisCharm):
     # OpenvSwitch and OVN is distributed as part of the Ubuntu Cloud Archive
     # Pockets get their name from OpenStack releases
     release = 'ussuri'
     name = 'ovn-chassis'
+
+    # Setting an empty source_config_key activates special handling of release
+    # selection suitable for subordinate charms
+    source_config_key = ''
